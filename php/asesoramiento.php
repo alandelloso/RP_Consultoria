7fb62bb8de93d9f344bf455830285e0ef4d5d184
alandelloso <alandelloso@gmail.com>
2014-08-10 01:28:37 -0300

    Mejoras PHP (Noticias)
    • Añadido PHP Mailer a  la sección de noticias mejorando el
    funcionamiento de los formularios

--- php/asesoramiento.php
+++ php/asesoramiento.php
@@ -5,15 +5,24 @@ $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['mail'];
 $mensaje = $_POST['comment'];
-$contenido = "
-Nombre Completo: $nombre $apellido 
-E-mail: $email
-Consulta: 
-$mensaje";
+$contenido = "Nombre: $nombre $apellido
+Mail: $email
+Mensaje: $mensaje";
+
+require "phpmailer/class.phpmailer.php";
+
+$mail = new PHPMailer;
+$mail->From = $email;
+$mail->FromName = "RP Website";
+$mail->Subject = $asunto;
+$mail->addAddress ($destino, 'RP Consultoria');
+$mail->Body = $contenido;
+
 if ($nombre == "" or $apellido == "" or $email == "" or $mensaje == "") {
 	header('Location: ../fail');
+} elseif ($mail->Send()) {
+	header('Location: ../success'); 
 } else {
-mail($destino, $asunto, $contenido);
-header('Location: ../success'); 
+	header('Location: ../fail');
 }
 ?>
