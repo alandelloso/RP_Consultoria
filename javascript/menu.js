58ce2656b23c952b3290b3e8171ac9de939d1c41
alandelloso <alandelloso@gmail.com>
2014-08-31 11:52:49 -0300

    Paises, Diseño, Nav Fix
    • Agregados los paises a las busquedas
    • Extendido el diseño renovado a todas las secciones de la página
    • Nav fix al hacer scroll

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,8 +1,24 @@
 $(document).on('ready', app);
 
 function app() {
+//Eventos
 	$('#menu').on('click', openMenu);
 	$('#cerrar').on('click', closeMenu);
+
+//Funciones Anónimas
+	$(window).scroll(function() {
+
+    var height = $(window).scrollTop();
+    if(height  > 100) {
+
+         $('nav').css({'position': 'fixed', 'top': '0', 'width': '100%'})
+         $('#tsocial').fadeIn();
+    } else if (height  < 100) {
+    	$('nav').css({'position': 'relative',})
+    	$('#tsocial').fadeOut();
+    }
+});
+
 }
 
 function openMenu() {
@@ -12,3 +28,4 @@ function openMenu() {
 function closeMenu() {
 	$('#mnav').css('left','-15em');
 }
+
