dac5cf4b511552dc396ba3ceb55ed25227ac1183
alandelloso <alandelloso@gmail.com>
2014-11-23 17:29:48 -0300

    Corrección Nav Flotante
    Corrección del nav flotante para versión tablet

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,30 +2,37 @@ $(document).on('ready', app);
 
 function app() {
 //Eventos
-	$('#menu').on('click', openMenu);
-	$('#cerrar').on('click', closeMenu);
+    $('#menu').on('click', openMenu);
+    $('#cerrar').on('click', closeMenu);
 
 //Funciones Anónimas
-	$(window).scroll(function() {
+    $(window).scroll(function() {
 
     var height = $(window).scrollTop();
-    if(height  > 100) {
+    var width = $(window).width();
 
+    if(width > 1165 && height  > 100) {
          $('nav').css({'position': 'fixed', 'top': '0', 'width': '100%'})
          $('#tsocial').fadeIn();
-    } else if (height  < 100) {
-    	$('nav').css({'position': 'relative',})
-    	$('#tsocial').fadeOut();
+    } else if (width > 1165 && height  < 100) {
+        $('nav').css({'position': 'relative',})
+        $('#tsocial').fadeOut();
+    } else if (width < 1165 && height  > 50) {
+        $('nav').css({'position': 'fixed', 'top': '0', 'width': '100%'})
+        $('#tsocial').fadeIn();
+    } else if (width < 1165 && height  < 50) {
+       $('nav').css({'position': 'relative',})
+       $('#tsocial').fadeOut();
     }
 });
 
 }
 
 function openMenu() {
-	$('#mnav').css('left',0);
+    $('#mnav').css('left',0);
 }
 
 function closeMenu() {
-	$('#mnav').css('left','-15em');
+    $('#mnav').css('left','-15em');
 }
 
